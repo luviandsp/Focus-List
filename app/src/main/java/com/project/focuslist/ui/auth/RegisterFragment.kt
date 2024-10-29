22f0317ff3e04559df490d990f9b3f079e37d4a6
Viannn <luvdasyapra@gmail.com>
2024-10-29 22:34:14 +0700

    Revert "Edit Auth UI"
    This reverts commit 0133633c7391c0d131d162e2c9dc4db1b19f826a.

--- app/src/main/java/com/project/focuslist/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/project/focuslist/ui/auth/RegisterFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
@@ -68,8 +67,6 @@ class RegisterFragment : Fragment() {
                 // Register user
                 val newUser = User(userId = 0, username = username, password = password)
                 viewModel.createUser(newUser)
-
-                Toast.makeText(requireContext(), "Registrasi berhasil", Toast.LENGTH_SHORT).show()
 
                 // Navigate to login
                 view?.findNavController()?.navigate(R.id.register_to_login)
